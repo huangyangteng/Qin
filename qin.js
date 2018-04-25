eaddab33c16a11884240706990c353ca9fbbf568
yellowyellow <yellowyellow12138@gmail.com>
2018-04-26 00:44:11 +0800

    添加$()方法，根据id获取元素

--- qin.js
+++ qin.js
@@ -16,4 +16,26 @@
         return true;
     }
     window['Q']['isCompatible'] = isCompatible;
+    // 添加$方法  相当于document.getElementById()
+    function $() {
+        var elements=new Array();
+        // 查找作为参数提供的所有元素
+        for (var i = 0, l = arguments.length; i < l; i++) {
+            var element=arguments[i];
+            if(typeof element==='string'){
+                element=document.getElementById(element);
+            }
+            // 如果只提供了一个参数，返回它
+            if(arguments.length===1){
+                return element;
+            }
+            // 否则，添加到数组中
+            elements.push(element);
+            
+        }
+        // 返回多个被请求元素的数组
+        return elements;
+
+    }
+    window['Q']['$']=$;
 })()
