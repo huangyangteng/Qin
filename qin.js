8e62e5ad22f6f4591aef2477f73e80cc1c54808c
yellowyellow <yellowyellow12138@gmail.com>
2018-04-26 00:35:41 +0800

    添加兼容性检查

--- qin.js
+++ qin.js
@@ -3,4 +3,17 @@
     if(!window.Q){
         window['Q']={};
     }
+    // 检查是否兼容
+    function isCompatible(other) {
+        if(other===false 
+            || !Array.prototype.push
+            || !Object.hasOwnProperty
+            || !document.createElement
+            ||!document.getElementsByTagName
+        ){
+            return false;
+        }
+        return true;
+    }
+    window['Q']['isCompatible'] = isCompatible;
 })()
